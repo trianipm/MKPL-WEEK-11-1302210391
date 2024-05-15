bb40de5aef85f44a72b2c2f6e7f3edb0d3641fad
trianipm <127008970+trianipm@users.noreply.github.com>
2024-05-15 19:34:32 +0700

    Mengubah nilai count 0 menjadi 1
    Pada class Counter.java mengubah inisialisasi count menjadi 1 secara langsung, ini akan mempengaruhi hasil pengujian yang mengharapkan count dimulai dari 0.

--- Basic-Java-Maven-main/src/main/java/Counter.java
+++ Basic-Java-Maven-main/src/main/java/Counter.java
@@ -1,27 +1,25 @@
-package src.main.java;
-
 public class Counter {
 
 	private int count;
-	
+
 	public Counter() {
 		this.reset();
 	}
-		
+
 	public void reset() {
-		count = 0;
+		count = 1;
 	}
-	
+
 	public void increment() {
 		count++;
 	}
-	
+
 	public void decrement() {
 		count--;
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
-	
+
 }
