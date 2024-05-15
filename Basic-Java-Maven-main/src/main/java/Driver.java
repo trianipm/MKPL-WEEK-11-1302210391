b476f74c3dcaf4b23718ae21bec8efd9787854f9
trianipm <127008970+trianipm@users.noreply.github.com>
2024-05-15 18:51:06 +0700

    Initial commit

---
+++ Basic-Java-Maven-main/src/main/java/Driver.java
@@ -0,0 +1,19 @@
+public class Driver {
+
+	public static void main(String[] args) {
+
+		Counter counter = new Counter();
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.increment();
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.decrement();
+
+		System.out.println("Count:" + counter.getCount());
+
+	}
+
+}
